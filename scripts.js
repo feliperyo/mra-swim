913fd89cdf6c8bbd2f133bd64edb09cd1e2484e7
feliperyo <ryofeliperyo@gmail.com>
2024-04-25 14:03:24 -0300

    adicionando animação

--- scripts.js
+++ scripts.js
@@ -11,3 +11,17 @@ function closeForm() {
     form.style.left = '-350px'
     mask.style.visibility = 'hidden'
 }
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element))
